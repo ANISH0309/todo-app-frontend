3bfe082bbb139d6cd30c51eb8015ade5c46233e4
ANISH0309 <mittalanish789@gmail.com>
2023-08-02 05:26:43 +0530

    fix: routing
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Landing from "./pages/Landing";
+import Register from "./pages/Register";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				{/* <h2>What up</h2> */}
+				<Routes>
+					<Route exact path="/register" element={<Register />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/" element={<Landing />} />
+				</Routes>
+			</div>
+		);
+	}
+}
+
+export default App;
